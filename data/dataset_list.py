b44449efb48f93edf3792bc1551bd9fd456f0fd8
TiagoCumetti <149095779+TiagoCumetti@users.noreply.github.com>
2024-04-21 20:57:40 +0200

    Update dataset_list.py
    I added the list of the new datasets with their respective frequency

--- data/dataset_list.py
+++ data/dataset_list.py
@@ -12,4 +12,69 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ALL_DATASETS = ["australian_electricity_demand", "electricity_hourly", "london_smart_meters_without_missing", "solar_10_minutes", "wind_farms_without_missing", "pedestrian_counts", "uber_tlc_hourly", "traffic", "kdd_cup_2018_without_missing", "saugeenday", "sunspot_without_missing", "exchange_rate", "cpu_limit_minute", "cpu_usage_minute", "function_delay_minute", "instances_minute", "memory_limit_minute", "memory_usage_minute", "platform_delay_minute", "requests_minute", "ett_h1", "ett_h2", "ett_m1", "ett_m2", "beijing_pm25", "AirQualityUCI", "beijing_multisite"]
+ALL_DATASETS = [ #27
+    "australian_electricity_demand", 
+    "electricity_hourly", 
+    "london_smart_meters_without_missing", 
+    "solar_10_minutes", 
+    "wind_farms_without_missing", 
+    "pedestrian_counts", 
+    "uber_tlc_hourly", 
+    "traffic", 
+    "kdd_cup_2018_without_missing", 
+    "saugeenday", 
+    "sunspot_without_missing", 
+    "exchange_rate", 
+    "cpu_limit_minute", 
+    "cpu_usage_minute", 
+    "function_delay_minute", 
+    "instances_minute", 
+    "memory_limit_minute", 
+    "memory_usage_minute", 
+    "platform_delay_minute", 
+    "requests_minute", 
+    "ett_h1", 
+    "ett_h2", 
+    "ett_m1", 
+    "ett_m2", 
+    "beijing_pm25", 
+    "AirQualityUCI", 
+    "beijing_multisite"
+]
+
+ALL_DATASETS_NEW_REPOSITORY = [ #34 (Tot: 61)
+    'car_parts_dataset_without_missing_values',              # Monthly,     equal length
+    'cif_2016_dataset',                                      # Monthly,            
+    'covid_deaths_dataset',                                  # Daily,       equal length
+    'covid_mobility_dataset_without_missing_values',         # Daily,       
+    'dominick_dataset',                                      # Weekly,      
+    'elecdemand_dataset',                                    # half_hourly, equal length
+    'electricity_weekly_dataset',                            # Weekly,      equal length
+    'fred_md_dataset',                                       # Monthly,     equal length
+    'hospital_dataset',                                      # Monthly,     equal length
+    'kaggle_web_traffic_dataset_without_missing_values',     # Daily,       equal length
+    'kaggle_web_traffic_weekly_dataset',                     # Weekly,      equal length
+    'm1_monthly_dataset',                                    # Monthly,
+    'm1_quarterly_dataset',                                  # Quarterly,  
+    'm1_yearly_dataset',                                     # Yearly,      
+    'm3_monthly_dataset',                                    # Monthly,     
+    'm3_quarterly_dataset',                                  # Quarterly,   
+    'm3_yearly_dataset',                                     # Yearly,      
+    'm4_daily_dataset',                                      # Daily,      
+    'm4_hourly_dataset',                                     # Hourly,      
+    'm4_monthly_dataset',                                    # Monthly,     
+    'm4_quarterly_dataset',                                  # Quarterly,  
+    'm4_weekly_dataset',                                     # Weekly,     
+    'm4_yearly_dataset',                                     # Yearly,      
+    'nn5_daily_dataset_without_missing_values',              # Daily,       equal length
+    'nn5_weekly_dataset',                                    # Weekly,      equal length
+    'solar_4_seconds_dataset',                               # 4_seconds,   equal length
+    'solar_weekly_dataset',                                  # Weekly,      equal length
+    'tourism_monthly_dataset',                               # Monthly,     
+    'tourism_quarterly_dataset',                             # Quarterly,   
+    'tourism_yearly_dataset',                                # Yearly,      
+    'traffic_weekly_dataset',                                # Weekly,      equal length
+    'us_births_dataset',                                     # Daily,       equal length
+    'weather_dataset',                                       # Daily,  
+    'wind_4_seconds_dataset',                                # 4_seconds,   equal length
+]
